25376b1411191e203b9bf8666701cf10149851aa
Alec Wilson <alecw1992@gmail.com>
2020-12-23 00:54:25 +0000

    Day 2 part 2

---
+++ 2020/day2/part2.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <iostream>
+#include <tuple>
+#include <string>
+#include <algorithm>
+
+std::tuple<int, int, char> parseConstraint(auto constraint)
+{
+    auto char_count_delimmiter = constraint.find_first_of("-");
+    return {
+        std::stoi(constraint.substr(0, char_count_delimmiter)),
+        std::stoi(constraint.substr(char_count_delimmiter + 1, constraint.size() - 4)),
+        constraint[constraint.size() - 2]
+    };
+}
+
+int main(int argc, char* argv[])
+{
+    std::fstream passwords("data/passwords.txt", std::ios_base::in);
+
+    std::string line, constraint, password;
+    std::size_t delimmiter_pos;
+    std::tuple<int, int, char> parsed_constraint;
+    auto valid_password_count = 0;
+    while (std::getline(passwords, line)) {
+        delimmiter_pos = line.find_last_of(" ");
+        constraint = line.substr(0, delimmiter_pos);
+        password = line.substr(delimmiter_pos + 1);
+        parsed_constraint = parseConstraint(constraint);
+        if ((password[std::get<0>(parsed_constraint) - 1] == std::get<char>(parsed_constraint)) ^ (password[std::get<1>(parsed_constraint) - 1] == std::get<char>(parsed_constraint))) {
+            ++valid_password_count;
+        }
+    }
+
+    std::cout << valid_password_count << std::endl;
+    return 0;
+}
+
